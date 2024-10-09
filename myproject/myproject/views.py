e8130db29c6c4b916f9bbe078a48a67a6188076b
vincent anjiri <vincentanjiri12@gmail.com>
2024-10-09 10:59:48 +0300

    first commit

---
+++ myproject/myproject/views.py
@@ -0,0 +1,9 @@
+# from django.http import HttpResponse
+from django.shortcuts import render
+def homepage(request):
+  # return HttpResponse("Hello, world! I'm Home")
+  return render(request, 'home.html')
+
+def about(request):
+  # return HttpResponse("This is the about page")
+  return render(request, 'about.html')
